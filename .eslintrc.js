862615d4357372efb8d848b1a903d65cf480ac2a
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-05-15 19:17:20 +0200

    Dependencies updated, template code from initial files deleted

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,26 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
+    "plugin:vue/vue3-essential",
+    "@vue/airbnb",
+    "@vue/typescript/recommended",
+    "prettier",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "template-curly-spacing": ["error", "never"],
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
         jest: true,
       },
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    "^.+\\.vue$": "vue-jest",
   },
+  collectCoverage: true,
+  testEnvironment: "jsdom",
+  collectCoverageFrom: ["src/**/*.{js,vue,ts}", "!src/main.js"],
+  modulePathIgnorePatterns: [
+    "src/main.ts",
+    "src/store/index.ts",
+    "shims-vue.d.ts",
+    "/node_modules/",
+  ],
+  verbose: true,
 };
