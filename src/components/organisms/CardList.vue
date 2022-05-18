75c9f423fde75d7bc4547f4c208cf2beddace902
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-05-18 02:11:46 +0200

    Component was refactored to use Composition API and useBreakpoints

--- src/components/organisms/CardList.vue
+++ src/components/organisms/CardList.vue
@@ -1,7 +1,10 @@
 <template>
-  <HorizontalScroll :isMobile="$isMobile()">
+  <b-t tag="h1" size="xl">
+    Lista de regalos en modo de scroll {{ upToMediumBreakpoint ? "vertical" : "horizontal" }}
+  </b-t>
+  <HorizontalScroll :isMobile="upToMediumBreakpoint">
     <GiftCard
-      v-for="(group, index) in groups || []"
+      v-for="(group, index) in groups"
       :key="index"
       :groupName="group.groupName"
       :groupDescription="group.groupDescription"
@@ -11,33 +14,37 @@
 </template>
 
 <script lang="ts">
+import BaseTypography from "@/components/atoms/BaseTypography.vue";
 import HorizontalScroll from "@/components/atoms/HorizontalScroll.vue";
 import GiftCard from "@/components/molecules/GiftCard.vue";
-import { defineComponent } from "vue";
+import useBreakpoints from "@/composables/useBreakpoints";
+import { defineComponent, onMounted, Ref, ref } from "vue";
+
+interface IGroup {
+  groupName: string;
+  groupDescription: string;
+  groupTags: Array<string>;
+}
 
 export default defineComponent({
   name: "CardList",
-  components: { GiftCard, HorizontalScroll },
+  components: { GiftCard, HorizontalScroll, BT: BaseTypography },
 
-  data() {
+  setup() {
+    const { upToMediumBreakpoint } = useBreakpoints();
+    const groups: Ref<IGroup[]> = ref([]);
+
+    onMounted(() => {
+      fetch("/groups.json")
+        .then((response) => response.json())
+        .then((data) => {
+          groups.value = data.groups;
+        });
+    });
     return {
-      groups: [
-        {
-          groupName: "",
-          groupDescription: "",
-          groupTags: [],
-        },
-      ],
-      isMobile: false,
+      groups,
+      upToMediumBreakpoint,
     };
-  },
-
-  created() {
-    fetch("/groups.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.groups = data.groups;
-      });
   },
 });
 </script>
