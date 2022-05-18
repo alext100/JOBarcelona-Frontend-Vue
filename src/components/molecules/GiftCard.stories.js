74a72ecba2f3f711a77d77940d649d10b83612d6
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-05-18 14:55:01 +0200

    Stories for GiftCard component added

---
+++ src/components/molecules/GiftCard.stories.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { action } from "@storybook/addon-actions";
+import GiftCard from "./GiftCard.vue";
+
+export default {
+  title: "molecules/GiftCard",
+  component: GiftCard,
+  argTypes: {
+    groupName: {
+      String,
+      control: { type: "text" },
+      required: true,
+    },
+    groupDescription: {
+      String,
+      control: { type: "text" },
+      required: true,
+    },
+    groupTags: {
+      required: true,
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { GiftCard },
+  setup() {
+    return {
+      handleOnClick: action("actionToDispatchOnClick"),
+      args,
+    };
+  },
+  template: `<GiftCard v-bind="args" @handleOnClick="handleOnClick" />`,
+});
+
+export const GiftCardComponent = Template.bind({});
+GiftCardComponent.args = {
+  groupName: "Family",
+  groupDescription: "Lista de regalos para el grupo Family",
+  groupTags: ["Family", "Sport"],
+};
